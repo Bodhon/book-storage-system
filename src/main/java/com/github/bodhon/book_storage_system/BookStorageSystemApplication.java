042931b9b339314dbd98ecba8812596c254933cc
Bodhon <maksimenkovkirill410@gmail.com>
2024-07-05 14:16:53 +0400

    add initial template

---
+++ src/main/java/com/github/bodhon/book_storage_system/BookStorageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.github.bodhon.book_storage_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStorageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStorageSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/bodhon/book_storage_system/BookStorageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bodhon.book_storage_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStorageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
